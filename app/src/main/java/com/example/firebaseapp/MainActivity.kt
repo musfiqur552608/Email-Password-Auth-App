37157cd66b01c2fe94848a04394e35de66191a2f
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-08-22 20:41:05 +0600

    Email-Password-Auth-App

---
+++ app/src/main/java/com/example/firebaseapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.firebaseapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.firebaseapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        binding.loginBtn.setOnClickListener {
+            auth.createUserWithEmailAndPassword(
+                binding.emailEtxt.text.toString(), binding.passwordEtxt.text.toString()
+            ).addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    Toast.makeText(this, task.result.toString(), Toast.LENGTH_LONG).show()
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                    Toast.makeText(
+                        this,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                }
+            }
+        }
+    }
+}
