778e3f04958000c193288d18122fe4d54d16b8ce
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-08-25 23:28:05 +0600

    Image added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,9 +49,13 @@ dependencies {
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.1")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.7.1")
+    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
     implementation("com.google.android.gms:play-services-auth:20.6.0")
+
+    implementation("com.squareup.picasso:picasso:2.8")
 }
--- app/src/main/java/com/example/firebaseapp/PhotoUploadActivity.kt
+++ app/src/main/java/com/example/firebaseapp/PhotoUploadActivity.kt
@@ -1,11 +1,40 @@
 package com.example.firebaseapp
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.firebaseapp.databinding.ActivityPhotoUploadBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.squareup.picasso.Picasso
 
 class PhotoUploadActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityPhotoUploadBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_photo_upload)
+        binding = ActivityPhotoUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.uploadBtn.setOnClickListener {
+            val intent = Intent()
+            intent.action=Intent.ACTION_PICK
+            intent.type="image/*"
+            imageLauncher.launch(intent)
+        }
+    }
+    val imageLauncher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode==Activity.RESULT_OK){
+            if (it.data!=null){
+                val ref = Firebase.storage.reference.child("photo")
+                ref.putFile(it.data!!.data!!).addOnSuccessListener {
+                    ref.downloadUrl.addOnSuccessListener {
+                        binding.imageView.setImageURI(it)
+                        Picasso.get().load(it.toString()).into(binding.imageView)
+                    }
+                }
+            }
+        }
     }
 }
