b0afa682c73d62cb17e167778c7b92b87418f379
Your NameMusfiqur <you@example.commushfiqur15-10872@diu.edu.bd>
2023-08-28 22:48:28 +0600

    progressbar

--- app/src/main/java/com/example/firebaseapp/VideoActivity.kt
+++ app/src/main/java/com/example/firebaseapp/VideoActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firebaseapp
 
 import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -19,12 +20,14 @@ import com.squareup.picasso.Picasso
 
 class VideoActivity : AppCompatActivity() {
     private lateinit var binding:ActivityVideoBinding
+    private lateinit var progressDialog:ProgressDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityVideoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.videoView.isVisible = false
+        progressDialog = ProgressDialog(this)
         binding.button.setOnClickListener {
             val intent = Intent()
             intent.action = Intent.ACTION_PICK
@@ -36,10 +39,13 @@ class VideoActivity : AppCompatActivity() {
     val videoLauncher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
         if(it.resultCode== Activity.RESULT_OK){
             if (it.data!=null){
+                progressDialog.setTitle("Uploading...")
+                progressDialog.show()
                 val ref = Firebase.storage.reference.child("Video/"+System.currentTimeMillis()+"."+getFileType(it.data!!.data))
                 ref.putFile(it.data!!.data!!).addOnSuccessListener {
                     ref.downloadUrl.addOnSuccessListener {
                         Firebase.database.reference.child("Video").push().setValue(it.toString())
+                        progressDialog.dismiss()
                         Toast.makeText(this@VideoActivity,"Video Uploaded", Toast.LENGTH_SHORT).show()
                         binding.button.isVisible = false
                         binding.videoView.isVisible = true
@@ -48,8 +54,17 @@ class VideoActivity : AppCompatActivity() {
                         binding.videoView.setVideoURI(it)
                         binding.videoView.setMediaController(mediaController)
                         binding.videoView.start()
+                        binding.videoView.setOnCompletionListener {
+                            ref.delete().addOnSuccessListener {
+                                Toast.makeText(this@VideoActivity, "Deleted", Toast.LENGTH_SHORT).show()
+                            }
+                        }
                     }
                 }
+                    .addOnProgressListener {
+                        val value=(it.bytesTransferred/it.totalByteCount)*100
+                        progressDialog.setTitle("Uploaded"+value.toString()+"%")
+                    }
 
             }
         }
